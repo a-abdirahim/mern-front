df9e1c2f205182dc2f22d9ecd63faf74fde0aecf
a-abdirahim <cabtamido55@gmail.com>
2023-12-31 09:42:22 +0300

    removed the scss rollup package

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import scss from "rollup-plugin-scss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), scss()],
+  plugins: [react()],
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
