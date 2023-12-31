d18fb83c77631ef72c28b156206cb30cbcbf0ec8
a-abdirahim <cabtamido55@gmail.com>
2023-12-31 08:50:25 +0300

    fixing scss rollup error

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import scss from "rollup-plugin-scss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), scss()],
   optimizeDeps: {
     exclude: ["js-big-decimal"],
   },
